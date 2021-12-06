b02ffaa93a63950bc386dae2a36b47613f786406
Ivan Ramirez <cancamilor@gmail.com>
2021-12-06 15:24:24 -0500

    Add jwt encrypt

--- app.py
+++ app.py
@@ -7,37 +7,59 @@ from flask import Response
 
 from format_checker import validate_response
 from zip_creator import create_zip
+from response_encoder import encode_response, decode_response
 
 
 app = Flask(__name__)
 
 
+@app.route("/")
+def hello_world():
+    return "<p>Go to postman!</p>"
+
+
+@app.route("/decode")
+def decode():
+    data = request.get_json()
+    response = data.get("response", "")
+    user_email = data.get("user_email", "")
+    return decode_response(response=response, user_email=user_email)
+
+
 @app.route("/get_character", defaults={'character_id': None})
 @app.route("/get_character/<int:character_id>")
 def hello(character_id):
+
+    # try:
     data = request.get_json()
     filter = data.get("filter", "")
+    email = data.get("email", "")
     is_zip_required = data.get("zip_required", False)
+    is_encoded_response = data.get("encoded", False)
+    format = "json"
 
     path = f"https://rickandmortyapi.com/api/{filter}"
     if character_id or character_id == 0:
         path += f"/{character_id}"
 
-    try:
-        response = requests.get(path)
+    response = requests.get(path)
 
-        json_response = validate_response(response)
-    
-        if json_response.get("error") or response.status_code != 200:
-            raise Exception(json_response.get("error"))
-    
-        if is_zip_required:
-            return create_zip(response=response, filter_value=filter)
-    
-        return json_response
+    final_response = validate_response(response)
 
-    except Exception as e:
-        return f"Error: {str(e)}"
+    if final_response.get("error") or response.status_code != 200:
+        raise Exception(final_response.get("error"))
+
+    if is_encoded_response:
+        final_response = encode_response(response=final_response, user_email=email)
+        format = "txt"
+
+    if is_zip_required:
+        return create_zip(response=final_response, filter_value=filter, format=format)
+
+    return final_response
+
+    # except Exception as e:
+    #     return f"Error: {str(e)}"
 
 
 if __name__ == "__main__":
--- zip_creator.py
+++ zip_creator.py
@@ -5,16 +5,16 @@ import time
 from flask import send_file
 
 
-def create_zip(response, filter_value):
+def create_zip(response, filter_value, format):
     filter_value = "data" if not filter_value else filter_value
 
     memory_file = io.BytesIO()
     with zipfile.ZipFile(memory_file, "w") as zf:
-        file_name = f"{filter_value}.json"
+        file_name = f"{filter_value}.{format}"
         data = zipfile.ZipInfo(file_name)
         data.date_time = time.localtime(time.time())[:6]
         data.compress_type = zipfile.ZIP_DEFLATED
-        zf.writestr(data, response.content)
+        zf.writestr(data, response)
 
     memory_file.seek(0)
 
